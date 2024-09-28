079c9349813d0785d92138a3f2987d5e06a25877
Beatriz-Chagas <bchagas440@gmail.com>
2024-09-28 03:12:36 -0300

    ✅ FACCAT 29

--- JS/FACCAT/27 - 38 Seleção Aninhada ou Concatenada/29 - Soma dos dois maiores/script29.js
+++ JS/FACCAT/27 - 38 Seleção Aninhada ou Concatenada/29 - Soma dos dois maiores/script29.js
@@ -1,6 +1,6 @@
 // Disciplina  : [L�gica de Programa��o com JavaScript]
 // Professor   : Jailson Santos
-// // Descri��o  : Ler 3 valores (considere que n�o ser�o informados valores iguais) e escrever o maior deles.
+// Descri��o  :  Ler 3 valores (considere que não serão informados valores iguais) e escrever a soma dos 2 maiores. 
 // Autor(a)    : Beatriz Chagas
 
 const form = document.querySelector('.formulario');
@@ -13,8 +13,15 @@ form.addEventListener('submit', (e) => {
   const numero2 = parseFloat(document.querySelector('.num2').value);
   const numero3 = parseFloat(document.querySelector('.num3').value);
 
-  const maior = Math.max(numero1, numero2, numero3);
+  const numeros = [ numero1, numero2, numero3];
 
-  result.textContent = `  O maior valor inserido foi: ${maior}`
+  numeros.sort((x, y) => y - x);
+
+  const doisMaiores = numeros.slice(0, 2);
+
+  const soma = doisMaiores[0] +  doisMaiores[1];
+
+
+  result.innerText = `A soma dos dois maiores valores é: ${soma}`;
 
 });
