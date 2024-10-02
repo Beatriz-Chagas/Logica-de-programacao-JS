06d196a5ea2546b02a6be9680c2817e37f63962c
Beatriz-Chagas <bchagas440@gmail.com>
2024-10-02 13:01:44 -0300

    ✅ FACCAT 48

--- JS/FACCAT/44 - 49 Estruturas de Repetição/48 - MEDIA DO ALUNO/script48.js
+++ JS/FACCAT/44 - 49 Estruturas de Repetição/48 - MEDIA DO ALUNO/script48.js
@@ -1,36 +1,35 @@
 // Disciplina  : [L�gica de Programa��o com JavaScript]
 // Professor   : Jailson Santos
-// Descri��o  : 48) Faça um algoritmo para ler um número que é um código de usuário. Caso este código seja
-// diferente de um código armazenado internamente no algoritmo (igual a 1234) deve ser apresentada a
-// mensagem ‘Usuário inválido!’. Caso o Código seja correto, deve ser lido outro valor que é a senha. Se
-// esta senha estiver incorreta (a certa é 9999) deve ser mostrada a mensagem ‘senha incorreta’. Caso a
-// senha esteja correta, deve ser mostrada a mensagem ‘Acesso permitido’.
+// Descri��o  : 48) Escreva um algoritmo para ler as notas da 1a. e 2a. avaliações de um aluno, calcule e imprima a
+// média (simples) desse aluno. Só devem ser aceitos valores válidos durante a leitura (0 a 10) para cada nota
 // Autor(a)    : Beatriz Chagas
 
 const form = document.querySelector('.formulario');
 const result = document.querySelector('.result');
-const senhaInput = document.querySelector('.num2');
+const nota1Input = document.querySelector('.nota1');
+const nota2Input = document.querySelector('.nota2');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('Evento submit disparado!');
 
-  const codigo = parseFloat(document.querySelector('.num1').value);
+  let nota1 = Number(nota1Input.value);
+  let nota2 = Number(nota2Input.value);
 
-  if (codigo === 1234) {
-    result.innerText = `O usuário está correto!`;
-    senhaInput.style.display = 'block';
-  } else {
-    result.innerText = `Usuário inválido!`;
+  if (nota1 < 0 || nota1 > 10) {
+    result.innerText = 'Nota 1 inválida';
+    nota1Input.focus();
+    return;
   }
-});
-
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const senha = parseFloat(senhaInput.value);
-  if (senha === 9999) {
-    result.innerText = `Acesso permitido!`;
-  } else {
-    result.innerText = `Senha ou usuário incorretos!`;
+  
+  nota2Input.style.display = 'block';
+  if (nota2 < 0 || nota2 > 10) {
+    result.innerText = 'Nota 2 inválida';
+    nota2Input.focus();
+    return;
+    
   }
+if  (nota1 > 0 && nota1 < 10 && nota2 > 0 && nota2 < 10) {
+  const total = (nota1 + nota2) / 2;
+  result.innerText = `A média final deste aluno(a) é: ${total}`;
+}
 });
