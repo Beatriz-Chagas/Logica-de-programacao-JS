4cfce17ef76548d1d1a068918e052d866d0cb1bf
Beatriz-Chagas <bchagas440@gmail.com>
2024-10-02 13:12:01 -0300

    ✅ FACCAT 49

--- JS/FACCAT/44 - 49 Estruturas de Repetição/49 - MEDIA DO ALUNO CONTINUAR/script49.js
+++ JS/FACCAT/44 - 49 Estruturas de Repetição/49 - MEDIA DO ALUNO CONTINUAR/script49.js
@@ -1,36 +1,47 @@
 // Disciplina  : [L�gica de Programa��o com JavaScript]
 // Professor   : Jailson Santos
-// Descri��o  : 49) Faça um algoritmo para ler um número que é um código de usuário. Caso este código seja
-// diferente de um código armazenado internamente no algoritmo (igual a 1234) deve ser apresentada a
-// mensagem ‘Usuário inválido!’. Caso o Código seja correto, deve ser lido outro valor que é a senha. Se
-// esta senha estiver incorreta (a certa é 9999) deve ser mostrada a mensagem ‘senha incorreta’. Caso a
-// senha esteja correta, deve ser mostrada a mensagem ‘Acesso permitido’.
+// Descri��o  : 49)Acrescente uma mensagem 'NOVO CÁLCULO (S/N)?' ao final do exercício [48]. Se for
+// respondido 'S' deve retornar e executar um novo cálculo, caso contrário deverá encerrar o algoritmo.
 // Autor(a)    : Beatriz Chagas
-
 const form = document.querySelector('.formulario');
 const result = document.querySelector('.result');
-const senhaInput = document.querySelector('.num2');
+const nota1Input = document.querySelector('.nota1');
+const nota2Input = document.querySelector('.nota2');
 
 form.addEventListener('submit', (e) => {
   e.preventDefault();
-  console.log('Evento submit disparado!');
 
-  const codigo = parseFloat(document.querySelector('.num1').value);
+  let nota1 = Number(nota1Input.value);
+  let nota2 = Number(nota2Input.value);
 
-  if (codigo === 1234) {
-    result.innerText = `O usuário está correto!`;
-    senhaInput.style.display = 'block';
-  } else {
-    result.innerText = `Usuário inválido!`;
+  if (nota1 < 0 || nota1 > 10) {
+    result.innerText = 'Nota 1 inválida';
+    nota1Input.focus();
+    return;
   }
-});
 
-form.addEventListener('submit', (e) => {
-  e.preventDefault();
-  const senha = parseFloat(senhaInput.value);
-  if (senha === 9999) {
-    result.innerText = `Acesso permitido!`;
-  } else {
-    result.innerText = `Senha ou usuário incorretos!`;
+  nota2Input.style.display = 'block';
+  if (nota2 < 0 || nota2 > 10) {
+    result.innerText = 'Nota 2 inválida';
+    nota2Input.focus();
+    return;
   }
+
+  if  (nota1 > 0 && nota1 < 10 && nota2 > 0 && nota2 < 10) {
+    const total = (nota1 + nota2) / 2;
+    result.innerText = `A média final deste aluno(a) é: ${total}`;
+  
+  // Acrescenta a mensagem "NOVO CÁLCULO (S/N)?" ao final do exercício
+  const novoCalculo = prompt('NOVO CÁLCULO (S/N)?');
+
+  if (novoCalculo.toUpperCase() === 'S') {
+    // Limpa os campos e permite que o usuário realize um novo cálculo
+    nota1Input.value = '';
+    nota2Input.value = '';
+    result.innerText = '';
+  } else {
+    // Encerra o algoritmo
+    result.innerText = 'Fim do algoritmo.';
+    form.removeEventListener('submit', this);
+  }}
 });
--- README.md
+++ README.md
@@ -9,7 +9,7 @@
 - 14 ao 26 :white_check_mark:
 - 27 ao 38 :white_check_mark:
 - 40 ao 43 :white_check_mark:
-- 44 ao 49 
+- 44 ao 49 :white_check_mark:
  ### Manzano
 - Lista 1
 - Extras
