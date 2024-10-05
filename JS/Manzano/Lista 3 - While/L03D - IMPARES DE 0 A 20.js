728888a867fc0424bdb66f083ceca0ab804896d0
Beatriz-Chagas <bchagas440@gmail.com>
2024-10-05 15:28:26 -0300

    ✅ Manzano 3D

---
+++ JS/Manzano/Lista 3 - While/L03D - IMPARES DE 0 A 20.js
@@ -0,0 +1,14 @@
+// Descri��o   : Apresentar todos os valores num�ricos inteiros �mpares situados na faixa de 0 a 20. Para verificar
+// se o n�mero � �mpar, efetuar dentro da malha a verifica��o l�gica desta condi��o com a instru��o
+// se, perguntando se o n�mero � �mpar; sendo, mostre-o; n�o sendo, passe para o pr�ximo passo.
+
+
+let contador = 0;
+      
+   while (contador < 20) { 
+      contador++
+
+       if (contador % 2 != 0) {
+      console.log(contador)};
+      
+};
