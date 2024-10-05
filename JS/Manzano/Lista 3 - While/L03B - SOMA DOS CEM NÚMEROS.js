e6f992f5b745a2c10f79bdb94b58dd00137264f8
Beatriz-Chagas <bchagas440@gmail.com>
2024-10-05 03:02:43 -0300

    ✅ Manzano 3B

---
+++ JS/Manzano/Lista 3 - While/L03B - SOMA DOS CEM NÚMEROS.js
@@ -0,0 +1,9 @@
+let somador = 0;
+let contador = 1;
+
+while (contador <= 100) {
+    somador += contador;
+    contador++;
+}
+
+console.log ("A soma de todos os números de 1 a 100 é: " + somador);
